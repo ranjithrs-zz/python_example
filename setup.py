799d6e58202ff47c28673f2d22109cee1f32a97b
Ranjith Ramakrishnan <ranjithrs@gmail.com>
2020-02-27 22:07:22 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='python-package-example',
+    name='python-example',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='An example python package',
     long_description=open('README.txt').read(),
     install_requires=['numpy'],
-    url='https://github.com/BillMills/python-package-example',
-    author='Bill Mills',
+    url='https://github.com/ranjithrs/python-example',
+    author='',
     author_email='myemail@example.com'
 )
